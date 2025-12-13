f2f3f3f14c3a45406c17119114fb0f29ad446780
maikel <m4ikel79@gmail.com>
2025-12-13 03:58:50 +0100

    feat: update icons with nerd fonts

--- lua/volt/icons.lua
+++ lua/volt/icons.lua
@@ -9,30 +9,128 @@ local icon_cache = {}
 
 -- Lightweight icon data (commonly used)
 local icons_by_extension = {
-  lua = { icon = "", color = "#51A0CF", name = "Lua" },
-  js  = { icon = "", color = "#EAD41C", name = "JavaScript" },
-  ts  = { icon = "", color = "#2b7489", name = "TypeScript" },
-  py  = { icon = "", color = "#3572A5", name = "Python" },
-  md  = { icon = "", color = "#083fa1", name = "Markdown" },
-  rs  = { icon = "", color = "#dea584", name = "Rust" },
-  go  = { icon = "", color = "#00ADD8", name = "Go" },
-  toml= { icon = "", color = "#9c4221", name = "TOML" },
-  json= { icon = "ﬥ", color = "#cbcb41", name = "JSON" },
-  sh  = { icon = "", color = "#6e4a7e", name = "Shell" },
-}
+  -- Programming Languages
+  lua   = { icon = "", color = "#51A0CF", name = "Lua" },
+  js    = { icon = "", color = "#CBCB41", name = "JavaScript" },
+  ts    = { icon = "", color = "#519ABA", name = "TypeScript" },
+  jsx   = { icon = "", color = "#20C2E3", name = "JavaScriptReact" },
+  tsx   = { icon = "", color = "#519ABA", name = "TypeScriptReact" },
+  py    = { icon = "", color = "#3572A5", name = "Python" },
+  rs    = { icon = "", color = "#DEA584", name = "Rust" },
+  go    = { icon = "", color = "#00ADD8", name = "Go" },
+  c     = { icon = "", color = "#599EFF", name = "C" },
+  cpp   = { icon = "", color = "#519ABA", name = "Cpp" },
+  h     = { icon = "", color = "#A074C4", name = "H" },
+  hpp   = { icon = "", color = "#A074C4", name = "Hpp" },
+  java  = { icon = "", color = "#CC3E44", name = "Java" },
+  rb    = { icon = "", color = "#701516", name = "Ruby" },
+  php   = { icon = "", color = "#A074C4", name = "Php" },
+  vim   = { icon = "", color = "#019833", name = "Vim" },
+
+  -- Markup & Data
+  html  = { icon = "", color = "#E44D26", name = "Html" },
+  css   = { icon = "", color = "#663399", name = "Css" },
+  scss  = { icon = "", color = "#F55385", name = "Scss" },
+  sass  = { icon = "", color = "#F55385", name = "Sass" },
+  json  = { icon = "", color = "#CBCB41", name = "JSON" },
+  yaml  = { icon = "", color = "#6D8086", name = "Yaml" },
+  yml   = { icon = "", color = "#6D8086", name = "Yml" },
+  toml  = { icon = "", color = "#9C4221", name = "TOML" },
+  xml   = { icon = "", color = "#E37933", name = "Xml" },
+  md    = { icon = "", color = "#519ABA", name = "Markdown" },
+
+  -- Shell & Config
+  sh    = { icon = "", color = "#4D5A5E", name = "Shell" },
+  bash  = { icon = "", color = "#89E051", name = "Bash" },
+  zsh   = { icon = "", color = "#F15BB5", name = "Zsh" },
+  fish  = { icon = "󰈺", color = "#4DB6AC", name = "Fish" },
+  conf  = { icon = "", color = "#6D8086", name = "Conf" },
+  env   = { icon = "", color = "#FAF743", name = "Env" },
+
+  -- Database
+  sql   = { icon = "", color = "#DAD8D8", name = "Sql" },
+  db    = { icon = "", color = "#DAD8D8", name = "Db" },
+
+  -- Images
+  png   = { icon = "", color = "#A074C4", name = "Png" },
+  jpg   = { icon = "", color = "#A074C4", name = "Jpg" },
+  jpeg  = { icon = "", color = "#A074C4", name = "Jpeg" },
+  gif   = { icon = "", color = "#A074C4", name = "Gif" },
+  svg   = { icon = "󰜡", color = "#FFB13B", name = "Svg" },
+  ico   = { icon = "", color = "#CBCB41", name = "Ico" },
+
+  -- Archives
+  zip   = { icon = "", color = "#ECA517", name = "Zip" },
+  tar   = { icon = "", color = "#ECA517", name = "Tar" },
+  gz    = { icon = "", color = "#ECA517", name = "Gz" },
+  rar   = { icon = "", color = "#ECA517", name = "Rar" },
+  ["7z"]= { icon = "", color = "#ECA517", name = "7z" },
+
+  -- Documents
+  pdf   = { icon = "", color = "#B30B00", name = "Pdf" },
+  doc   = { icon = "󰈬", color = "#185ABD", name = "Doc" },
+  docx  = { icon = "󰈬", color = "#185ABD", name = "Docx" },
+  txt   = { icon = "", color = "#89E051", name = "Text" },
+},
 
 local icons_by_filename = {
-  [".gitignore"] = { icon = "", color = "#F54D27", name = "GitIgnore" },
-  ["makefile"]   = { icon = "", color = "#6D8086", name = "Makefile" },
-  ["dockerfile"] = { icon = "", color = "#0db7ed", name = "Dockerfile" },
-  ["readme.md"]  = { icon = "", color = "#083fa1", name = "Readme" },
-  ["license"]    = { icon = "", color = "#6d8086", name = "License" },
-  ["package.json"]= { icon = "", color = "#cbcb41", name = "Npm" },
-  ["tsconfig.json"]= { icon = "", color = "#2b7489", name = "TSConfig" },
-  ["cargo.toml"] = { icon = "", color = "#dea584", name = "Cargo" },
-  [".env"]       = { icon = "", color = "#4f5d95", name = "Env" },
-  ["init.lua"]   = { icon = "", color = "#51A0CF", name = "LuaInit" },
+  -- Git
+  [".gitignore"]      = { icon = "", color = "#F54D27", name = "GitIgnore" },
+  [".gitattributes"]  = { icon = "", color = "#F54D27", name = "GitAttributes" },
+  [".gitconfig"]      = { icon = "", color = "#F54D27", name = "GitConfig" },
+  [".gitmodules"]     = { icon = "", color = "#F54D27", name = "GitModules" },
+
+  -- Docker
+  ["dockerfile"]         = { icon = "󰡨", color = "#458EE6", name = "Dockerfile" },
+  [".dockerignore"]     = { icon = "󰡨", color = "#458EE6", name = "DockerIgnore" },
+  ["docker-compose.yml"]= { icon = "󰡨", color = "#458EE6", name = "DockerCompose" },
+
+  -- Package Managers
+  ["package.json"]       = { icon = "", color = "#E8274B", name = "PackageJson" },
+  ["package-lock.json"]  = { icon = "", color = "#7A0D21", name = "PackageLockJson" },
+  ["yarn.lock"]          = { icon = "", color = "#2C8EBB", name = "YarnLock" },
+  ["pnpm-lock.yaml"]     = { icon = "", color = "#F9AD02", name = "PNPMLock" },
+  ["cargo.toml"]         = { icon = "", color = "#DEA584", name = "Cargo" },
+  ["go.mod"]             = { icon = "", color = "#00ADD8", name = "GoMod" },
+  ["pyproject.toml"]     = { icon = "", color = "#3572A5", name = "PyProject" },
+  ["requirements.txt"]   = { icon = "", color = "#3572A5", name = "Requirements" },
+
+  -- Config Files
+  ["tsconfig.json"]      = { icon = "", color = "#519ABA", name = "TSConfig" },
+  ["webpack.config.js"]  = { icon = "󰜫", color = "#519ABA", name = "Webpack" },
+  ["vite.config.js"]     = { icon = "", color = "#FFA800", name = "ViteConfig" },
+  [".eslintrc"]          = { icon = "󰱺", color = "#4B32C3", name = "Eslintrc" },
+  [".prettierrc"]        = { icon = "󰱺", color = "#4285F4", name = "PrettierConfig" },
+  [".editorconfig"]      = { icon = "", color = "#FFF2F2", name = "EditorConfig" },
+
+  -- Build Tools
+  ["makefile"]           = { icon = "", color = "#6D8086", name = "Makefile" },
+  ["cmakelists.txt"]     = { icon = "", color = "#DCE3EB", name = "CMakeLists" },
+  ["justfile"]           = { icon = "", color = "#6D8086", name = "Justfile" },
+
+  -- Shell Config
+  [".bashrc"]            = { icon = "", color = "#89E051", name = "Bashrc" },
+  [".zshrc"]             = { icon = "", color = "#89E051", name = "Zshrc" },
+  [".bash_profile"]      = { icon = "", color = "#89E051", name = "BashProfile" },
+
+  -- Editor Config
+  [".vimrc"]             = { icon = "", color = "#019833", name = "Vimrc" },
+  ["init.lua"]           = { icon = "", color = "#51A0CF", name = "LuaInit" },
+  ["init.vim"]           = { icon = "", color = "#019833", name = "VimInit" },
+
+  -- Env & Secrets
+  [".env"]               = { icon = "", color = "#FAF743", name = "Env" },
+  [".env.local"]         = { icon = "", color = "#FAF743", name = "Env" },
+  [".env.example"]       = { icon = "", color = "#FAF743", name = "Env" },
+
+  -- Documentation
+  ["readme.md"]          = { icon = "󰂺", color = "#EDEDED", name = "Readme" },
+  ["readme"]             = { icon = "󰂺", color = "#EDEDED", name = "Readme" },
+  ["license"]            = { icon = "󰿃", color = "#D0BF41", name = "License" },
+  ["license.md"]         = { icon = "󰿃", color = "#D0BF41", name = "License" },
+  ["changelog.md"]       = { icon = "", color = "#EDEDED", name = "Changelog" },
 }
+
 
 -- Default fallback icon
 local default_icon = {
